67bb346c2f9fb99f866b7ff39132b41e165d9e36
Eduardo Pereira Ribeiro <eduardo.6899@outlook.com>
2022-12-19 19:05:00 -0300

    Remoção da rota para deleção de todos os usuários

--- routes.js
+++ routes.js
@@ -6,7 +6,6 @@ rota.post("/user/criar", userController.create);
 rota.patch("/user/atualizar/:id", userController.update);
 rota.get("/user/recuperar", userController.getAll);
 rota.get("/user/recuperar/:id", userController.getById);
-rota.delete("/user/remover/", userController.removeAll);
 rota.delete("/user/remover/:id", userController.removeById);
 
 module.exports = rota;
